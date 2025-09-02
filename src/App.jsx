af4437317cf7f237f0dc7eaa8fbb5199badb2e15
Rafael Barbosa <rafael.barbosa51@gmail.com>
2025-09-01 22:49:21 -0700

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "@/pages";
 
 export default function App() {
   return (
-    <div className="h-screen bg-blue-200">
-
+    <div className="w-full h-screen bg-gradient-to-r from-stone-500 to-stone-700">
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/ludmila-bday2025/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+gsap.registerPlugin(useGSAP)
+import { useRef } from "react"
+import { ChevronDown } from "lucide-react"
+
+export default function Home(){
+    const navigate = useNavigate()
+    const containerRef = useRef()
+    const pageRef = useRef()
+    useGSAP(() => {
+        gsap.fromTo(pageRef.current, {x: '100vw', opacity:0}, {x: '0', opacity: 1, duration: 2})
+    }, {scope: containerRef})
+    return (
+        <div ref={containerRef}>
+            <div ref={pageRef} className="relative w-full h-screen flex flex-col justify-center items-center font-waterbrush bg-gradient-to-r from-blue-200 to-cyan-200">
+                <h1 className="text-7xl text-red-500">Para mi esposa</h1>
+                <ChevronDown className="absolute bottom-4 text-red-500" size={96} 
+                onClick={() => navigate('/second')}
+                />
+            </div>
+        </div>
+    )
+}
